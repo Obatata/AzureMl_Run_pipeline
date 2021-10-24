23eb57198da3e62a1d255f05db9d3e981db7c3a8
Obatata <batata.oussama@gmail.com>
2021-10-24 21:07:12 +0200

    first commit

---
+++ pipeline.py
@@ -0,0 +1,106 @@
+from azureml.core import Workspace
+from azureml.core import Environment
+from azureml.core.environment import  CondaDependencies
+from azureml.core.compute import AmlCompute
+from azureml.core.compute import ComputeTarget
+from azureml.core.runconfig import RunConfiguration
+from azureml.pipeline.steps import  PythonScriptStep
+from azureml.pipeline.core import PipelineData
+from azureml.pipeline.core import  Pipeline
+from azureml.core import Experiment
+
+
+"""
+Access the workspace
+"""
+ws = Workspace.from_config("./config")
+"""
+###############################################################################
+"""
+
+
+"""
+Create custom envirnmont
+"""
+# create the envirnmont
+pipeline_env = Environment(name="pipeline_env")
+
+# creae the dependencies object
+conda_depends = CondaDependencies.create(conda_packages=(["sklearn", "pandas", "azureml-sdk"]))
+pipeline_env.python.conda_dependencies = conda_depends
+
+# set user_manager_dependencies to True
+pipeline_env.python.user_managed_dependencies = True
+
+# register the envirnmont
+pipeline_env.register(ws)
+"""
+###############################################################################
+"""
+
+"""
+create compute cluster for pipeline
+"""
+# give name of the cluster
+cluster_name = "pipeline-cluster"
+
+# set the configuration of th cluster
+compute_config = AmlCompute.provisioning_configuration(
+                                                       vm_size='STANDARD_D11_V2',
+                                                       max_nodes=2
+                                                      )
+# biuld comute cluster
+compute_cluster = ComputeTarget.create(ws, cluster_name, compute_config)
+
+# wait for completion
+compute_cluster.wait_for_completion()
+"""
+###############################################################################
+"""
+
+
+"""
+Create Run Configuration for the steps 
+"""
+run_config = RunConfiguration()
+run_config.target = compute_cluster
+run_config.environment = pipeline_env
+"""
+###############################################################################
+"""
+
+
+"""
+Pipeline steps 
+"""
+input_ds = ws.datasets.get("Defaults")
+datafolder = PipelineData("datafolder", datastore=ws.get_default_datastore())
+
+# Step 1 : Data peprocessing :
+dataPreprocess_step = PythonScriptStep(
+                                        name="01 Data preprocessing",
+                                        source_directory=".",
+                                        script_name="preprocessing_data_step.py",
+                                        inputs=[input_ds.as_named_input("raw_data")],
+                                        outputs=[datafolder],
+                                        runconfig=run_config,
+                                        arguments=["--datafolder", datafolder]
+                                      )
+
+# Configure and build the pipeline
+steps = [dataPreprocess_step]
+new_pipeline = Pipeline(
+                        workspace=ws,
+                        steps=steps
+                       )
+
+# create the experiment and run pipeline
+new_experiment = Experiment(
+                            workspace=ws,
+                            name="PipelineExp01"
+                           )
+new_pipeline_run = new_experiment.submit(new_pipeline)
+new_pipeline_run.wait_for_completion(show_output=True)
+"""
+###############################################################################
+"""
---
+++ preprocessing_data_step.py
@@ -0,0 +1,83 @@
+from azureml.core import Run
+import pandas as pd
+from argparse import ArgumentParser as AP
+from sklearn.preprocessing import MinMaxScaler
+
+
+"""
+Get the run context 
+Recall : we are in the sub-script (submited by the pipeline job)
+"""
+new_run = Run.get_context()
+"""
+###############################################################################
+"""
+
+
+"""
+Get the workspace from the run 
+"""
+ws = new_run.experiment.workspace
+"""
+###############################################################################
+"""
+
+
+"""
+Preprocessing data 
+"""
+# read input data
+#df = new_run.input_dataset["raw_data"].to_pandas_dataframe
+df = pd.read_csv("./data/Loan+Approval+Prediction.csv")
+# remove ID coolumn (dont need Id column for build pipeline)
+data_preproc = df.drop(["ID"], axis=1)
+
+#get columns of dataframe
+all_cols = data_preproc.columns
+
+# get missing values as vector of sum
+dataNull = data_preproc.isnull().sum()
+
+# replace missing values of string variables (columns) with mode method
+mode = data_preproc.mode().iloc[0]
+cols = data_preproc.select_dtypes(include="object").columns
+data_preproc[cols] = data_preproc[cols].fillna(mode)
+
+# replace missing values of string variables (columns) with mean
+mean = data_preproc.mean()
+data_preproc = data_preproc.fillna(mean)
+
+# create dummy variables (1-hot encoding)
+data_preproc = pd.get_dummies(data_preproc, drop_first=True)
+
+# normalize data
+scaler = MinMaxScaler()
+columns = df.select_dtypes(include="number").columns
+data_preproc[columns] = scaler.fit_transform(data_preproc[columns])
+
+# get the arguments from the pipline job
+parser = AP()
+parser.add_argument("--datafolder", type=str)
+args = parser.parse_args()
+
+# create folder if does not exist
+import os
+os.makedev(args.datafolder, exist_ok=True)
+
+# create the path
+path = os.path.join(args.datafolder, "defaults_prep.csv")
+
+# write the data_preproc ad csv file
+data_preproc.to_csv(path, index=False)
+"""
+###############################################################################
+"""
+
+
+"""
+Complete the run 
+"""
+new_run.complete()
+"""
+###############################################################################
+"""
